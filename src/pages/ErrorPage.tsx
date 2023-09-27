ed104fadeead13bc4bf337afc779671bca1449b5
KimCookieYa <min49590@gmail.com>
2023-09-27 15:08:18 +0900

    Feat: ErroPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -9,7 +9,10 @@ export default function ErrorPage() {
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>
+          {(error as Error)?.message ||
+            (error as { statusText?: string })?.statusText}
+        </i>
       </p>
     </div>
   );
