36e0f8d65a11c6fa785bbb2b1f710595daa5eeda
KimCookieYa <min49590@gmail.com>
2023-09-28 03:15:02 +0900

    Feat: subscribe/unsubscribe 구혐 && 몽고DB 연결

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ import axios from "axios";
 import xml2js from "xml2js";
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -15,7 +17,35 @@ const __dirname = path.resolve();
 const app = express();
 const port = 3000;
 
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  latest: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Email = mongoose.model("Email", UserSchema);
+
 // 정적 파일 제공
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../dist")));
 
 // 루트 엔드포인트에서 React 앱을 서비스
@@ -23,7 +53,38 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
-app.post("/api/user/subscribe", (req, res) => {});
+app.post("/api/user/subscribe", async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: "이메일 주소가 필요합니다." });
+  }
+
+  try {
+    // 이메일을 MongoDB에 저장
+    const newEmail = await new Email({ email });
+    await newEmail.save();
+    res.json({ message: "이메일 저장 성공" });
+    const emails = await Email.find(); // 모든 이메일 정보 조회
+    console.log(emails);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "서버 오류" });
+  }
+});
+
+// 이메일 삭제 엔드포인트
+app.delete("/api/user/unsubscribe/:email", async (req, res) => {
+  const { email } = req.params;
+
+  try {
+    // 이메일을 MongoDB에서 삭제
+    await Email.deleteOne({ address: email });
+    res.json({ message: "이메일 삭제 성공" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "서버 오류" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
+import axios from "axios";
 
 function Main() {
   const [email, setEmail] = useState<string>("");
   const [subscribed, setSubscribed] = useState<boolean>(false);
 
-  const handleSubscribe = () => {
-    // 여기에서 서버 또는 데이터베이스에 구독 요청을 보내는 로직을 구현할 수 있습니다.
-    // 이 예제에서는 간단히 subscribed 상태를 토글합니다.
-    setSubscribed(!subscribed);
+  const handleSubscribe = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      if (!subscribed) {
+        await axios.post("/api/user/subscribe", { email });
+        console.log("Email sent and saved to the database");
+        setSubscribed(true);
+      } else {
+        await axios.delete(`/api/user/unsubscribe/${email}`);
+        console.log("Email removed from the database");
+        setSubscribed(false);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-semibold mb-4">
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="p-8 bg-white rounded-lg shadow-lg">
+        <h2 className="mb-4 text-2xl font-semibold">
           {subscribed ? "구독 중입니다" : "메일링 서비스 구독"}
         </h2>
         {!subscribed ? (
@@ -21,13 +33,13 @@ function Main() {
             <input
               type="email"
               placeholder="이메일을 입력하세요"
-              className="px-3 py-2 border rounded-md flex-grow"
+              className="flex-grow px-3 py-2 border rounded-md"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
             <button
               onClick={handleSubscribe}
-              className="px-4 py-2 bg-blue-500 text-white rounded-md"
+              className="px-4 py-2 text-white bg-blue-500 rounded-md"
             >
               구독
             </button>
@@ -35,7 +47,7 @@ function Main() {
         ) : (
           <button
             onClick={handleSubscribe}
-            className="px-4 py-2 bg-red-500 text-white rounded-md"
+            className="px-4 py-2 text-white bg-red-500 rounded-md"
           >
             구독 취소
           </button>
