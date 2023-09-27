2d1a9ab3ab3dd97405d01134f9de003a2dd34ef1
KimCookieYa <min49590@gmail.com>
2023-09-27 14:00:42 +0900

    Design: install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (
-    <>
-      Hello, World!
-    </>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
-export default App
+export default App;
