d2c4db540a15e3d8426ecad4c1bb8b7d121ba7cc
KimCookieYa <min49590@gmail.com>
2023-09-29 02:54:45 +0900

    Design: NavBar 상단 고정

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,9 @@
 function NavBar() {
   return (
     <>
-      <div className="bg-blue-500 text-white p-4">
+      <div className="absolute w-full p-4 text-white bg-blue-500">
         <div className="container mx-auto">
-          <div className="flex justify-between items-center">
+          <div className="flex items-center justify-between">
             <div className="text-xl font-bold">PNU 메일 받아라</div>
           </div>
         </div>
