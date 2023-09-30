4e7d8d4e61c92f5c0399da834d531afe7927fdbb
KimCookieYa <min49590@gmail.com>
2023-10-01 03:36:45 +0900

    Design: NavBar 색상 변경

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 function NavBar() {
   return (
     <>
-      <div className="absolute w-full p-4 text-white bg-blue-500">
+      <div className="absolute w-full p-4 text-black bg-white">
         <div className="container mx-auto">
           <div className="flex items-center justify-between">
             <div className="text-xl font-bold">PNU 메일 받아라</div>
