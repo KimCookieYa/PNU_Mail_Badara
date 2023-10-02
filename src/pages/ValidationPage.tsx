40453a36736c5b1f051bc21cf609b8d86fbe10b7
KimCookieYa <min49590@gmail.com>
2023-10-03 02:55:23 +0900

    Design: 컴포넌트 width 변경

--- src/pages/ValidationPage.tsx
+++ src/pages/ValidationPage.tsx
@@ -5,7 +5,7 @@ export default function ValidationPage() {
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="p-8 space-y-2 bg-white rounded-lg shadow-lg">
+      <div className="w-9/12 p-8 space-y-2 bg-white rounded-lg shadow-lg">
         <p>
           {email}에 대한 검증이 완료되었습니다! 구독을 취소하시려면,{" "}
           <Link to="/" className="text-blue-500 underline">
