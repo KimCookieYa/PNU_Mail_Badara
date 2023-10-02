78a8503e9e753a6a188650abbfe595cc1b636bb7
KimCookieYa <min49590@gmail.com>
2023-10-03 02:56:04 +0900

    Chore: React 환경변수 적용

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -3,9 +3,10 @@ import "../App.css";
 function BottomBar() {
   return (
     <>
-      <div className="absolute bottom-0 left-0 w-full p-4 opacity-80">
-        학과/게시판 추가 요청 또는 개선사항이 있을 시, min49590@gmail.com으로
-        연락주시기 바랍니다:)
+      <div className="absolute bottom-0 left-0 w-full p-4 text-center opacity-80">
+        학과/게시판 추가 요청 또는 개선사항이 있을 시,{" "}
+        {import.meta.env.VITE_GOOGLE_MAIL_USER_ID}으로 연락주시기 바랍니다:)
+        <br />* 학과명, 학과 홈페이지 링크, 자주 보는 게시판 포함 *
       </div>
     </>
   );
