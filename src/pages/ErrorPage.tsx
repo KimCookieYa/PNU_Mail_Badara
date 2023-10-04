96ce0c4e069a18467c4b172b8410948583bd90d9
KimCookieYa <min49590@gmail.com>
2023-10-04 10:15:07 +0900

    Style: export default 통일

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,6 @@
 import { useRouteError } from "react-router-dom";
 
-export default function ErrorPage() {
+function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
@@ -17,3 +17,5 @@ export default function ErrorPage() {
     </div>
   );
 }
+
+export default ErrorPage;
