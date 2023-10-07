044d85dcedd8ae625c71a40280ce76eb62c983b6
KimCookieYa <min49590@gmail.com>
2023-10-07 15:30:46 +0900

    Design: 예시 메일 사진 변경

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,5 +1,5 @@
-import gmail from "../assets/gmail_screenshot.jpg";
-import gmail2 from "../assets/gmail_screenshot2.jpg";
+import gmail3 from "../assets/gmail_screenshot3.jpg";
+import gmail4 from "../assets/gmail_screenshot4.jpg";
 import { forwardRef } from "react";
 
 const DetailPage = forwardRef<HTMLDivElement>((__, ref) => {
@@ -8,8 +8,8 @@ const DetailPage = forwardRef<HTMLDivElement>((__, ref) => {
       ref={ref}
       className="flex flex-col items-center justify-center w-auto h-full lg:flex-row"
     >
-      <img src={gmail} alt="gmail" className="max-h-3/4" />
-      <img src={gmail2} alt="gmail2" className="max-h-3/4" />
+      <img src={gmail3} alt="gmail3" className="max-h-3/4" />
+      <img src={gmail4} alt="gmail4" className="max-h-3/4" />
     </div>
   );
 });
