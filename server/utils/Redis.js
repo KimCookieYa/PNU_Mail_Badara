8b4fae63ae6b8b133cde113b198ea27af66d8273
KimCookieYa <min49590@gmail.com>
2023-10-12 01:47:16 +0900

    Feat: docker:redis에서 redislabs로 변경

--- server/utils/Redis.js
+++ server/utils/Redis.js
@@ -1,7 +1,21 @@
 import Redis from "ioredis";
 
 export const initializeRedis = async () => {
-  const redisClient = new Redis(6379, "redis-server");
+  const port =
+    process.env.NODE_ENV === "production"
+      ? process.env.REDIS_LABS_PORT
+      : process.env.REDIS_DOCKER_PORT;
+  const host =
+    process.env.NODE_ENV === "production"
+      ? process.env.REDIS_LABS_HOST
+      : process.env.REDIS_DOCKER_HOST;
+  const options =
+    process.env.NODE_ENV === "production"
+      ? {
+          password: process.env.REDIS_LABS_PASSWORD,
+        }
+      : {};
+  const redisClient = new Redis(port, host, options);
   redisClient.on("connect", () => {
     console.log("[Success] Redis Connected");
   });
