04fab13fbebf5e42fdf323b82613c2fdfe4f1c06
KimCookieYa <min49590@gmail.com>
2024-01-02 01:04:10 +0900

    Design: 소개 페이지 디자인 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { routerInfoList } from "../pages/Router";
 function Header({ moveToArtist }: { moveToArtist: (index: number) => void }) {
   return (
     <>
-      <header className="fixed flex items-center justify-between w-full h-20">
+      <header className="fixed flex items-center justify-between w-full h-16 bg-gray-400 bg-opacity-80">
         <Title onClick={() => moveToArtist(0)} />
         <nav className="hidden md:flex">
           <ul className="flex mr-8 text-white gap-x-6">
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,35 +2,33 @@ import { forwardRef } from "react";
 
 const AboutPage = forwardRef<HTMLDivElement>((__, ref) => {
   return (
-    <section ref={ref} className="flex justify-center w-auto min-h-screen">
-      <div className="flex items-center justify-center w-auto m-10 mt-20 bg-white rounded-lg lg:m-20 lg:w-1/2 lg:flex-row h-3/4">
+    <section ref={ref} className="flex justify-center w-full min-h-screen">
+      <div className="flex items-center justify-center m-10 mt-20 bg-white rounded-lg lg:m-20 lg:w-1/2 lg:flex-row h-3/4">
         <div className="flex flex-col h-auto p-6 text-center">
-          <div className="m-6 text-4xl font-bold">Mail Badara</div>
+          <p className="m-6 text-4xl font-bold">Mail Badara</p>
           <br />
-          <div className="text-2xl italic text-gray-500">
-            하루 한 번, 오전 10시부터 1시간마다 학과마다 보내드립니다.
+          <p className="text-2xl italic text-gray-400">
+            하루에 한 번, <br />
+            학과 게시판 소식을 뉴스레터 형식으로 보내드립니다.
+          </p>
+          <br />
+          <br />
+          <p>
+            저는 부산대학교 전기컴퓨터공학부 정보컴퓨터공학전공 18학번
+            김민석이라고 합니다. 평소 학과 홈페이지의 게시판을 자주 뒤졌었는데
+            이게 너무 불편해서 직접 개발했습니다. 타과생, 타학교 학생들도 쓸 수
+            있게끔 설계하였습니다. 디자인은 갖다버리고 기능에만 집중하였습니다.
+            이 서비스가 도움이 되었으면 좋겠습니다:) 많이 쓰일수록 포폴로 쓰기
+            좋거든요.
+          </p>
+          <br />
+          <br />
+          <br />
+          <p>
+            학과/게시판 추가 요청 또는 개선사항이 있을 시,
             <br />
-            <br />
-            학과의 새 소식을 메일로 전달해드립니다:)
-          </div>
-          <br />
-          <br />
-          저는 부산대학교 전기컴퓨터공학부 정보컴퓨터공학전공 18학번
-          김민석이라고 합니다. 평소 학과 홈페이지의 게시판을 자주 뒤졌었는데
-          이게 너무 불편해서 직접 개발했습니다. 타과생, 타학교 학생들도 쓸 수
-          있게끔 설계하였습니다. 디자인은 갖다버리고 기능에만 집중하였습니다. 이
-          서비스가 도움이 되었으면 좋겠습니다:) 많이 쓰일수록 포폴로 쓰기
-          좋거든요.
-          <br />
-          <br />
-          <br />
-          <br />
-          학과/게시판 추가 요청 또는 개선사항이 있을 시,{" "}
-          {import.meta.env.VITE_GOOGLE_MAIL_USER_ID}으로 연락주시기 바랍니다!
-          그리고 학과 홈페이지에서 rss를 제공하지 않을 시, 추가되는데에 시간이
-          소요됩니다.
-          <br />
-          <br />* 학과명, 학과 홈페이지 링크, 자주 보는 게시판 포함 *<br />
+            {import.meta.env.VITE_GOOGLE_MAIL_USER_ID}으로 연락주시기 바랍니다!
+          </p>
           <br />
         </div>
       </div>
